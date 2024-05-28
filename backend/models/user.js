db19b894edfa48953b2aa21c19af04996a76c3b5
IqraYasir <iqrayasir0906@gmail.com>
2024-05-28 20:47:11 +0100

    Create model and schema for users

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: { type: String, maxLength: 70, required: true },
+    username: { type: String, maxLength: 40, required: true },
+    password: { type: String, maxLength: 40, required: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+UserSchema.virtual('blogger').get(function() {
+    if (this.posts.length === 0) {
+        return false;
+    } else {
+        return true;
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
