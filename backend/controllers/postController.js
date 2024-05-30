de278ef019a46d0bc5e08e86564d2de1a162b6a8
IqraYasir <iqrayasir0906@gmail.com>
2024-05-30 17:54:15 +0100

    Draft controllers for posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,26 +1,71 @@
 const Post = require('../models/post');
-const asyncHandler  = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
+const validationResult = require('express-validator');
 
 exports.post_list = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Post list');
+    const allPosts = await Post.find({})
+        .sort({ date_posted: 1 })
+        .populate('user')
+        .populate('comment')
+        .exec()
+
+    res.json(allPosts);
 });
 
-exports.post_create_get = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Create post GET');
+exports.post_create_post = asyncHandler(async (req, res, next) =>{
+    if (!Array.isArray(req.body.comments)) {
+        req.body.comments = typeof req.body.comments === 'undefined'
+        ? [] : [req.body.comments];
+    }
+
+    const errors = validationResult(req);
+    if (!errors.empty()) {
+        res.json(errors.array());
+    }
+
+    const createdPost = new Post({
+        user: req.body.user,
+        comments: typeof req.body.comments === 'undefined'
+        ? [] : req.body.comments,
+        title: req.body.title,
+        published: req.body.published,
+        text: req.body.text,
+        date_posted: Date.now() 
+    });
+    
+    createdPost.save();
+    res.json(createdPost);
 });
 
-exports.post_create_post = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Create post POST');
-});
 
 exports.postId_update = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: UPDATE post');
+    if (!Array.isArray(req.body.comments)) {
+        req.body.comments = typeof req.body.comments === 'undefined'
+        ? [] : [req.body.comments];
+    }
+
+    const errors = validationResult(req);
+    if (!errors.empty()) {
+        res.json(errors.array());
+    }
+
+    const updatedPost = Post.findByIdAndUpdate(req.params.id);
+    
+    updatedPost.save();
+    res.json(updatedPost);
 });
 
 exports.postId_delete = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: DELETE post');
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+
+    res.json(deletedPost);
 });
 
 exports.postId_get = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Get specific post');
+    const post = Post.findById(req.params.id)
+        .populate('user')
+        .populate('comment')
+        .exec()
+
+    res.json(post);
 });
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,8 +6,6 @@ router.get('/posts', post_controller.post_list);
 
 router.get('/posts/:postId', post_controller.postId_get);
 
-router.get('/post_form', post_controller.post_create_get);
-
 router.post('/post_form', post_controller.post_create_post);
 
 router.put('/posts/:postId', post_controller.postId_update);
