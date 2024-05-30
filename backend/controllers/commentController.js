cea8eaf82e30b6eb199c94f808e3b834209e4e64
IqraYasir <iqrayasir0906@gmail.com>
2024-05-30 17:31:35 +0100

    Draft controllers for comments, install packages

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,22 +1,46 @@
+const validationResult = require('express-validator');
 const Comment = require('../models/comment');
 const asyncHandler = require('express-async-handler');
 
 exports.comment_list = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Comment list');
-});
+    const allComments = await Comment.find({})
+        .sort({ date_posted: 1 })
+        .populate('user')
+        .exec()
 
-exports.comment_create_get = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Create comment GET');
+    res.json(allComments);
 });
 
 exports.comment_create_post = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: Create comment POST');
-});
+    const errors = validationResult(req);
+    if (!errors.empty()) {
+        res.json(errors.array());
+    }
+
+    const createdComment = new Comment({
+        user: req.body.user,
+        text: req.body.text,
+        date_posted: Date.now()
+    });
+
+    createdComment.save();
+    res.json(createdComment);
+})
 
 exports.commentId_update = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: UPDATE comment');
+    const errors = validationResult(req);
+    if (!errors.empty()) {
+        res.json(errors.array());
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(req.params.id);
+    
+    updatedComment.save();
+    res.json(updatedComment);
 });
 
 exports.commentId_delete = asyncHandler(async (req, res, next) => {
-    res.json('NOT IMPLEMENTED: DELETE comment');
+    const deletedComment = await Comment.findByIdAndDelete(req.params.id);
+
+    res.json(deletedComment);
 });
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -4,8 +4,6 @@ const comment_controller = require('../controllers/commentController');
 
 router.get('/posts/:postId/comments', comment_controller.comment_list);
 
-router.get('/comment_form', comment_controller.comment_create_get);
-
 router.post('/comment_form', comment_controller.comment_create_post);
 
 router.put('/comments/:commentId', comment_controller.commentId_update);
