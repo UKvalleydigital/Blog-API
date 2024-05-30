8fc603511c5e04f85bed23438ea5a00b7698abb7
IqraYasir <iqrayasir0906@gmail.com>
2024-05-30 17:11:26 +0100

    Add other fields and constraints

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User',  required: true},
-    body: { type: String, required: true },
-    date_posted: { type: Date }
+    text: { type: String, maxLength: 100, required: true },
+    date_posted: { type: Date,  required: true }
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- backend/models/post.js
+++ backend/models/post.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment', required: true }],
     title: { type: String, required: true },
     published: { type: Boolean, required: true },
-    body: { type:  String, required: true },
-    date_posted: { type: Date }
+    text: { type:  String, maxLength: 500, required: true },
+    date_posted: { type: Date, required: true }
 })
 
 module.exports = mongoose.model('Post', PostSchema)
