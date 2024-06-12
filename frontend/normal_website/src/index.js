8ee065398b1937c184b7047af211b5c303a6ff9e
IqraYasir <iqrayasir0906@gmail.com>
2024-06-12 20:56:58 +0100

    Abandon webpack

--- frontend/normal_website/src/index.js
+++
@@ -1,8 +0,0 @@
-function component() {
-    const element = document.createElement('p');
-
-    element.textContent = 'Hello world'
-    return element;
-}
-
-document.body.appendChild(component());
