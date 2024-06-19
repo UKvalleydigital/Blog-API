fadb5bbae65d6e6d7c0aa627ed4316abdb9e3df9
IqraYasir <iqrayasir0906@gmail.com>
2024-06-19 17:59:46 +0100

    Pass data between home and post api

--- frontend/scripts/elements/post.js
+++ frontend/scripts/elements/post.js
@@ -1,11 +1,19 @@
-const Post = function () {
+export default function Post () {
     let postList = [];
+    const returnPosts = () => postList;
 
     const createPost = () => {
         const list = document.createElement('ul');
         const point = document.createElement('li');
-
-        // Fetch input post data
-
     };
+
+    const getPosts = () => {
+        const url = `http://localhost:3000/posts`
+        fetch(url, { method: 'GET' })
+            .then(res => res.json())
+            .then(res => res.allPosts)
+            .then(res => postList.push(res));
+    };
+
+    return { createPost, getPosts, returnPosts };
 };
---
+++ frontend/scripts/home.js
@@ -0,0 +1,20 @@
+import Post from './elements/post.js';
+
+// Display posts
+function createList(list) {
+    if (list.length <=  0) return;
+    const ul = document.querySelector('ul');
+
+    list.forEach(element => {
+        const li = document.createElement('li');
+        li.textContent = element;
+        ul.appendChild(li);
+    });
+}
+
+Post().getPosts();
+const response = Post().returnPosts();
+response.push('post 4');
+
+createList(response);
+
