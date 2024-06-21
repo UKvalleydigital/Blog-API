0dc703db5fb02b3ebf0457b4873ec16327b39cfa
IqraYasir <iqrayasir0906@gmail.com>
2024-06-21 18:10:47 +0100

    Create header and update styling

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -3,7 +3,7 @@ import Post from './elements/post.js';
 // Display posts
 function createList(list) {
     if (list.length <=  0) return;
-    const ul = document.querySelector('ul');
+    const ul = document.querySelector('.post_list');
 
     list.forEach(element => {
         const li = document.createElement('li');
